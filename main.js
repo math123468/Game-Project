e567ec0f4102859b174e09ae10ca020065643edf
math123468 <44072698+math123468@users.noreply.github.com>
2021-04-22 12:58:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 var loc1 = [0,0]
 var loc2 = [0,0]
 var player = 1
+var nextVector = [0,0]
 function roll(times) {
 	var num1 = Math.floor(Math.random()*5)
 	var num2 = Math.floor(Math.random()*5)
 	document.getElementById('num1').innerHTML = num1
 	document.getElementById('num2').innerHTML = num2
-	if(times == 0) return [num1,num2]
+	if(times == 0) {
+		vector = [num1,num2]
+		return
+	}
 	var delay
 	if(times > 50) delay = 50
 	else if(times > 20) delay = 100
@@ -20,7 +24,7 @@ function roll(times) {
 function setUp() {
 	for(i=0;i<10;i++) {
 		for(j=0;j<10;j++) {
-			document.getElementById(i.toString() + j).onclick = function() {moveTo(i.toString() + j)}
+			document.getElementById(i.toString() + j).onclick = function(arg) {return function() {moveTo(arg);}}(10*i+j);
 		}
 	}
 }
@@ -28,17 +32,17 @@ function moveTo(square, override = false) {
 	if(document.getElementById(square).style.backgroundColor = 'black' || override) {
 		unhighlightAll()
 		if(player == 1) {
-			loc1[0] = square[0]
-			loc1[1] = square[1]
+			loc1[0] = square[0].toString()
+			loc1[1] = square[1].toString()
 		}
 		else if(player == 2) {
-			loc2[0] = square[0]
-			loc2[1] = square[1]
+			loc2[0] = square[0].toString()
+			loc2[1] = square[1].toString()
 		}
 	}
 }
 function move() {
-	var vector = roll(100)
+	roll(100)
 	var currentLoc
 	if(player == 1) currentLoc = loc1
 	else currentLoc = loc2
@@ -81,4 +85,5 @@ function setup() {
 	moveTo('00')
 	player = 1
 	moveTo('00')
+	setUp()
 }
